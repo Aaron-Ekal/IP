202a87f4c48e64dd4158a797bcf1df8aa908c4b3
Aaron Ekal <ekalaaronkalale@gmail.com>
2022-03-13 20:18:29 +0300

    add form validation

--- js/app.js
+++ js/app.js
@@ -43,4 +43,18 @@ $(document).ready(function(){
   }, function () {
     $('.companyTitle', this).slideToggle('fast');
   });
+
+   // Form Validation
+  $("#submit").click(function () {
+    
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+    let message = $("textarea#message").val();
+    if (name && email !== null) {
+      alert ("Hey " + name + ", Thank you for reaching out to us. One of our representatives will get back to you as soon as possible.");
+  }
+    else {
+      alert("Please provide your correct name and email!")
+  }
+  });
 })
