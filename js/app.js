bd3f74e874c60cdf2e5baac131e921c78755919a
Aaron Ekal <ekalaaronkalale@gmail.com>
2022-03-13 20:17:07 +0300

    add toggle

--- js/app.js
+++ js/app.js
@@ -3,4 +3,37 @@ $(document).ready(function(){
   $("#designDescription").hide()
   $("#developmentDescription").hide()
   $("#productDescription").hide()
+
+
+
+  // Crreating toggle functionality on what we do section
+  $("#designImage").click(function () {
+    $("#designImage").hide();
+    $("#designDescription").show();
+  });
+
+  $("#designDescription").click(function () {
+    $("#designDescription").hide();
+    $("#designImage").show();
+  });
+
+  $("#developmentImage").click(function () {
+    $("#developmentImage").hide();
+    $("#developmentDescription").show();
+  });
+
+  $("#developmentDescription").click(function () {
+    $("#developmentDescription").hide();
+    $("#developmentImage").show();
+  });
+
+  $("#productImage").click(function () {
+    $("#productImage").hide();
+    $("#productDescription").show();
+  });
+
+  $("#productDescription").click(function () {
+    $("#productDescription").hide();
+    $("#productImage").show();
+  });
 })
